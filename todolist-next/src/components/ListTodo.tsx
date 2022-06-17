9b8f4ef26acd2e868d61cd356536537365547047
Joao Vitor <joaomaximo22@hotmail.com>
2022-06-17 11:19:48 -0300

    feat: Next, adicionar icones no componente de tarefas

--- todolist-next/src/components/ListTodo.tsx
+++ todolist-next/src/components/ListTodo.tsx
@@ -1,8 +1,17 @@
-import { VStack,Box, IconButton,Text, Heading, Stack, Flex, Button, Spacer, Grid, HStack, Checkbox } from "@chakra-ui/react"
+import { VStack,Box, IconButton,Text, Heading, Stack, Flex, Button, Spacer, Grid, HStack, Checkbox, Editable, EditablePreview, EditableTextarea, Input } from "@chakra-ui/react"
 import axios from "axios"
 import {useEffect, useState} from 'react'
+
+import {HiPlus} from 'react-icons/hi'
+import {IoMdArrowBack,IoIosCloseCircle} from 'react-icons/io'
+import {RiMoreFill} from 'react-icons/ri'
+import {BsCheckCircleFill} from 'react-icons/bs'
+import {MdModeEdit} from 'react-icons/md'
+
+
 export function ListToDo({ value }){
     const [toDos,setToDos] = useState([])
+    const [isEdit,setIsEdit] = useState(false)
 
 useEffect(()=>{
     async function getToDos(){
@@ -24,26 +33,46 @@ return(
         <Grid width={'full'}>
         <HStack justifyContent={'space-between'} mt={20}>
             <Flex>
-                <Button color={'black'} borderRadius={'30%'} mr={2}>{'<'}</Button>
+                <IconButton aria-label="Voltar" color={'black'} borderRadius={'30%'} mr={2} icon={<IoMdArrowBack/>}/>
                 <Text fontSize={'xl'}>a</Text>
             </Flex>
             
             <Box>
-                <Button color={'black'}>...</Button>
+            <RiMoreFill fontSize='20px' cursor={'pointer'} />
+                
             </Box>
         </HStack>
 
         <HStack mt={10} w='full' >
-            <Box w='full' border={'2px solid  #1d1d26'} borderRadius={'15px'} p={2}>
-            <Button colorScheme='pink' size={'xs'} color={'black'} mr={2}>+</Button>
-            Add Task
+            <HStack w='full' border={'2px solid  #1d1d26'} borderRadius={'15px'} p={2}>
+            <Box>
+                <IconButton aria-label="Adicionar-tarefa" colorScheme='pink' size={'xs'} fontSize='16px' color={'black'} mr={2} icon={<HiPlus/>}/>
             </Box>
+            <Box>
+            Adicionar tarefa
+
+            </Box>
+            </HStack>
         </HStack>
 
         <HStack  mt={10} w='full'>
-            <Stack w='full'  borderRadius={'8px'} backgroundColor={'#21212b'} p={4}>
-                <Checkbox  borderRadius={2} defaultChecked={false}>Checkbox</Checkbox>
-            </Stack>
+            <HStack  w='full' borderRadius={'8px'} backgroundColor={'#21212b'} p={4} >
+                {isEdit?
+                <HStack w={'full'}>
+                    <Input defaultValue={'aaa'} placeholder={'To do...'}/>
+                  <IconButton colorScheme={'red'} aria-label="fechar-tarefa"  fontSize='20px' icon={<IoIosCloseCircle/>} onClick={(e)=>setIsEdit(false)}/>
+                  <IconButton colorScheme={'green'} aria-label="editar-tarefa"  fontSize='16px'icon={<BsCheckCircleFill/>} onClick={(e)=>setIsEdit(false)}/>
+                  
+                </HStack>
+                :
+                <HStack w='full' justifyContent={'space-between'}>
+                <Checkbox  borderRadius={2} defaultChecked={false} >
+                    Tarefa 1
+                </Checkbox>
+                <MdModeEdit onClick={(e)=>setIsEdit(true)} fontSize='20px' cursor={'pointer'}/>
+                </HStack>
+                }
+            </HStack>
         </HStack>
         </Grid>
    
